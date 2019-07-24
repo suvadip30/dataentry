2b3dd5a9973e4bf3e33d44cf7ea90c54b6b91d9e
root <root@ip-172-31-44-100.ec2.internal>
2019-07-24 14:49:22 +0000

    updated

--- dataentry.sh
+++ dataentry.sh
@@ -1,9 +1,17 @@
 #/bin/usr
-
+rm -rf data1.txt
+rm -rf data2.txt
+rm -rf data3.txt
 files='file.txt'
 
 
 for file in $files
 do
-    cat $file | grep "Commercial Name (English)" | sed 's/Commercial Name (English)/\n/g'
+    cat $file | grep "Commercial Name (English)" | sed 's/Commercial Name (English)/\n/g' >> data1.txt
+    cat $file | grep "Flat / Shop No." -A 2 >> data2.txt
+    cat $file | grep "Email" | sed 's/Email/\n/g' >> data3.txt
 done
+
+sed -i '/^$/d; s/^[ \t]*//' data1.txt
+awk '{printf $0 (NR%4?" ":"\n")}' data2.txt
+sed -i '/^$/d; s/^[ \t]*//; s/Mobile.*//' data3.txt
